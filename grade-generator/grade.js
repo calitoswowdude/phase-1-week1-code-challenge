78d824d49be7f7837eadb650cc90c881b307a6b9
Carl <carl.allan@student.moringaschool.com>
2022-11-29 11:27:33 +0300

    Added comments

--- grade-generator/grade.js
+++ grade-generator/grade.js
@@ -4,21 +4,27 @@ function studentGrade(marks){
     if (marks < 40){
       return "Grade : E";      
      } 
+   //   when user inputs a grade that is less than 40, the system will give an output of, "Grade E"
      else if (marks < 49) {
         return "Grade : D"; 
      } 
+   //   when user inputs a grade that is less than 49, the system will give an output of, "Grade D"
      else if (marks < 60) {
         return "Grade : C"; 
      } 
+   //   when user inputs a grade that is less than 60, the system will give an output of, "Grade C"
      else if (marks < 79) {
         return "Grade : B"; 
      }
+   //   when user inputs a grade that is less than 79, the system will give an output of, "Grade B"
      else if (marks <= 100) {
         return "Grade : A"; 
      }
+   //   when user inputs a grade that is less than or equal to 100, the system will give an output of, "Grade A"
 }
 
 function letterGrade(print) {
     let marks = document.getElementById("input").value
     document.getElementById("letter").textContent = studentGrade(marks)
 }
+// The function checks what the user has input and runs the input through the function studentGrade(marks), gets the returned value and spit it back to the html where it will display the return value to the user
--- speed-detector/speed.js
+++ speed-detector/speed.js
@@ -1,7 +1,10 @@
 function speedDetector(speed) {
     if (speed < 70) {
         return "Ok"
-    } else {
+    } 
+    // If the speed detected by the user is less than 70, the function returns a value, "Ok"
+    
+    else {
         let n = (speed - 70)/5;
         if (n>12) {
             return "License suspended";
@@ -10,9 +13,12 @@ function speedDetector(speed) {
         }
     
     }
+    // If the speed detected by the user is greater than 70, the function checks for the difference between the speed and 70. It then divides the difference by 5.
+    // If the result is more than 12, it will return a value of "License suspende". If it is less than 12, it will return a value of `points "(result)"`
 }
 
 function ticketPrinter(print) {
     let speed= document.getElementById("input").value
     document.getElementById("conclusion").textContent=speedDetector(speed)
 }
+// This function checks what the user has input and runs the input through the function speedDetector(speed), gets the returned value and spit it back to the html where it will display the return value to the user
